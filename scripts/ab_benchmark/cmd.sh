09c37d4b0a773ee0a62e1605646bef9a36fac24a
void-kun <hoangzrik@gmail.com>
2024-08-25 09:16:43 +0700

    Update benchmark

--- scripts/ab_benchmark/cmd.sh
+++ scripts/ab_benchmark/cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ab  -p post_body.json -T application/json -n 5000 -c 100 http://localhost:7000/api/auth/login
+go-wrk -M POST -H 'Content-Type: application/json' -body @post_body.json -c 2048 -d 10 http://localhost:7000/api/auth/login
